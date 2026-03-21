15876881d59818b075924121d6ee0f45d683b140
Sukhorukov Dmitry <09sde@mail.ru>
2026-03-21 16:53:49 +0500

    Add list_prof.html and requirements.txt

--- main.py
+++ main.py
@@ -10,5 +10,27 @@ def index():
 def index_with_title(title):
     return render_template('base.html', title=title)
 
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    PROFESSIONS = [
+    'Инженер-конструктор',
+    'Строитель',
+    'Биолог',
+    'Геолог',
+    'Физик',
+    'Химик',
+    'Врач',
+    'Психолог',
+    'Программист',
+    'Робототехник'
+]
+
+    return render_template('list_prof.html', 
+                         title='Список профессий',
+                         professions=PROFESSIONS,
+                         list_type=list_type)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
