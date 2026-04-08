b98b474799cd2654ab2d7ed410d3b39970f0b33b
Sukhorukov Dmitry <09sde@mail.ru>
2026-04-08 18:45:09 +0500

    Новое решение

---
+++ main4.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Mars Mission')
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    user_data = {
+        'title': 'Анкета участника миссии',
+        'surname': 'Иванов',
+        'name': 'Иван',
+        'education': 'Высшее техническое',
+        'profession': 'Инженер-исследователь',
+        'sex': 'Мужской',
+        'motivation': 'Всегда мечтал участвовать в космических исследованиях и вносить вклад в развитие науки',
+        'ready': 'Готов к любым испытаниям'
+    }
+    
+    return render_template('auto_answer.html', **user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
